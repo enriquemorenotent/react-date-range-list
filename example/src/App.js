9e3d667a5ce54101744968c46c9ee4ad9c3a80a1
Enrique Moreno Tent <enriquemorenotent@gmail.com>
2019-01-09 19:17:54 +0100

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExampleComponent from 'react-date-range-list'
+import DateRangePicker from "react-date-range-list";
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
+  render = () => (
+    <div className="test">
+      <DateRangePicker onChange={console.log} />
+      <DateRangePicker addButton={<span>Add a new entry</span>} />
+      <DateRangePicker removeButton={<span>Remove this entry</span>} />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,95 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import styles from './styles.css'
+import styles from "./styles.css";
 
-export default class ExampleComponent extends Component {
+export default class DateRangePicker extends Component {
   static propTypes = {
     text: PropTypes.string
-  }
+  };
 
-  render() {
-    const {
-      text
-    } = this.props
+  state = {
+    nextId: 0,
+    verb: "on",
+    date1: "",
+    date2: "",
+    validDate2: false,
+    results: []
+  };
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
+  isValid = () => this.state.date1 !== "";
+
+  renderAddButton = () => (
+    <span onClick={this.handleAddDateRange}>
+      {this.props.addButton ? this.props.addButton : <button>Add</button>}
+    </span>
+  );
+
+  renderRemoveButton = id => (
+    <span onClick={() => this.handleDeleteEntry(id)}>
+      {this.props.removeButton ? (
+        this.props.removeButton
+      ) : (
+        <button>Remove</button>
+      )}
+    </span>
+  );
+
+  handleSetVerb = e => this.setState({ verb: e.target.value });
+
+  handleSetDate = e =>
+    e.target.checkValidity() && this.setState({ date1: e.target.value });
+
+  handleAddDateRange = () =>
+    this.isValid()
+      ? this.setState(
+          {
+            results: [
+              ...this.state.results,
+              {
+                verb: this.state.verb,
+                date1: this.state.date1,
+                id: this.state.nextId
+              }
+            ],
+            nextId: this.state.nextId + 1
+          },
+          () => this.props.onChange(this.state.results)
+        )
+      : alert("There is an invalid value");
+
+  handleDeleteEntry = id =>
+    this.setState(
+      { results: this.state.results.filter(r => r.id !== id) },
+      () => () => this.props.onChange(this.state.results)
+    );
+
+  render = () => (
+    <div className={styles.DateRangePicker}>
+      <div className="form">
+        <select value={this.state.verb} onChange={this.handleSetVerb}>
+          <option value="on">On </option>
+          <option value="from">From</option>
+          <option value="until">Until</option>
+          <option value="between">Between</option>
+        </select>
+        &nbsp;
+        <input
+          type="date"
+          value={this.state.date1}
+          onChange={this.handleSetDate}
+        />
+        &nbsp;
+        {this.renderAddButton()}
       </div>
-    )
-  }
+      <ul className="list">
+        {this.state.results.map(item => (
+          <li className="item" key={item.id}>
+            {item.verb} {item.date1}
+            {this.renderRemoveButton(item.id)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
